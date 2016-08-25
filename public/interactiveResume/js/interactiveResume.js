7f76c6b21701fcca60fe411f5ae98ef18c64f027
Wyatt Barnes <wyattb413@gmail.com>
2016-08-25 08:03:53 -0500

    Basic layout of website completed Things TODO: add content, transition animation between content 'pages', add animation to general styling of webpage

--- public/interactiveResume/js/interactiveResume.js
+++ public/interactiveResume/js/interactiveResume.js
@@ -1,1 +1,36 @@
-console.log('linked');
+// (function(){
+"use strict";
+
+//==============================================================={Curve Text}======================================================================\
+
+$('#firstHeading').arctext({radius: 300})
+$('#secondHeading').arctext({radius: 300})
+$('#thirdHeading').arctext({radius: 300, dir: -1})
+$('#fourthHeading').arctext({radius: 300})
+
+//==================================================================================================================================================\
+
+//==============================================================={Click Functions}==================================================================\
+
+$('#myProjectsClick').click(function(){
+	$('#welcomeText').remove();
+	var myProjectTextDiv = $('#myProjectTextDiv').html(); 
+	$('#welcomeTextDiv').html(myProjectTextDiv);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// })();
