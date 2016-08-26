3c09dd0eec7a43283fd97bf72d3ff01dc7e28e25
Wyatt Barnes <wyattb413@gmail.com>
2016-08-25 20:14:17 -0500

    merge 71575d6d1363671af24ce8c06be2869aecc5bb06

--- public/interactiveResume/js/interactiveResume.js
+++ public/interactiveResume/js/interactiveResume.js
@@ -12,10 +12,39 @@ $('#fourthHeading').arctext({radius: 300})
 
 //==============================================================={Click Functions}==================================================================\
 
+$('#welcomeClick').click(function(){
+	$('#myProjectText').fadeOut();
+	$('#careerStoryText').fadeOut();
+	$('#contactMeText').fadeOut();
+	setTimeout(function(){
+		$('#welcomeText').fadeIn();
+	},500);
+})
 $('#myProjectsClick').click(function(){
-	$('#welcomeText').remove();
-	var myProjectTextDiv = $('#myProjectTextDiv').html(); 
-	$('#welcomeTextDiv').html(myProjectTextDiv);
+	$('#welcomeText').fadeOut();
+	$('#careerStoryText').fadeOut();
+	$('#contactMeText').fadeOut();
+	setTimeout(function(){
+	$('#myProjectText').fadeIn();
+	},500);
+	$('#calculatorVid').get(0).load();
+	$('#calculatorVid').get(0).play();
+})
+$('#careerStoryClick').click(function(){
+	$('#welcomeText').fadeOut();
+	$('#myProjectText').fadeOut();
+	$('#contactMeText').fadeOut();	
+	setTimeout(function(){
+	$('#careerStoryText').fadeIn();
+	},500);
+})
+$('#contactMeClick').click(function(){
+	$('#welcomeText').fadeOut();
+	$('#myProjectText').fadeOut();
+	$('#careerStoryText').fadeOut();
+	setTimeout(function(){
+	$('#contactMeText').fadeIn();	
+	},500);
 })
 
 
