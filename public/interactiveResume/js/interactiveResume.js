a762192c7c5bdfcb3b0b8c38a34f99460ec60a15
Wyatt Barnes <wyattb413@gmail.com>
2016-08-30 08:38:53 -0500

    Added colorClockVideo and left and right arrow menu buttons

--- public/interactiveResume/js/interactiveResume.js
+++ public/interactiveResume/js/interactiveResume.js
@@ -14,12 +14,17 @@ $('#fourthHeading').arctext({radius: 300})
 $('#myProjectText').hide();
 $('#careerStoryText').hide();
 $('#contactMeText').hide();
+$('#rightMenuArrow').hide();
+$('#leftMenuArrow').hide();
+$('#colorClockVideo').hide();
 
 
 $('#welcomeClick').click(function(){
 	$('#myProjectText').fadeOut();
 	$('#careerStoryText').fadeOut();
 	$('#contactMeText').fadeOut();
+	$('#rightMenuArrow').hide();
+	$('#leftMenuArrow').hide();
 	setTimeout(function(){
 		$('#welcomeText').fadeIn();
 	},500);
@@ -30,14 +35,18 @@ $('#myProjectsClick').click(function(){
 	$('#contactMeText').fadeOut();
 	setTimeout(function(){
 	$('#myProjectText').fadeIn();
+	$('#rightMenuArrow').fadeIn();
+	$('#leftMenuArrow').fadeIn();
 	},500);
-	$('#calculatorVid').get(0).load();
-	$('#calculatorVid').get(0).play();
+	$('#calcVideo').get(0).load();
+	$('#calcVideo').get(0).play();
 })
 $('#careerStoryClick').click(function(){
 	$('#welcomeText').fadeOut();
 	$('#myProjectText').fadeOut();
-	$('#contactMeText').fadeOut();	
+	$('#contactMeText').fadeOut();
+	$('#rightMenuArrow').hide();
+	$('#leftMenuArrow').hide();	
 	setTimeout(function(){
 	$('#careerStoryText').fadeIn();
 	},500);
@@ -46,12 +55,30 @@ $('#contactMeClick').click(function(){
 	$('#welcomeText').fadeOut();
 	$('#myProjectText').fadeOut();
 	$('#careerStoryText').fadeOut();
+	$('#rightMenuArrow').hide();
+	$('#leftMenuArrow').hide();
 	setTimeout(function(){
 	$('#contactMeText').fadeIn();	
 	},500);
 })
 
 
+$('#rightMenuArrow').click(function(){
+	$('#calcVideo').fadeOut();
+	setTimeout(function(){
+	$('#colorClockVideo').fadeIn();
+	},500);
+	$('#colorClockVideo').get(0).load();
+	$('#colorClockVideo').get(0).play();
+})
+$('#leftMenuArrow').click(function(){
+	$('#colorClockVideo').fadeOut();
+	setTimeout(function(){
+	$('#calcVideo').fadeIn();
+	},500);
+	$('#calcVideo').get(0).load();
+	$('#calcVideo').get(0).play();
+})
 
 
 
