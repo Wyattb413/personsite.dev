7df1c3f5124d4694864807a73480644cd6fe39da
Wyatt Barnes <wyattb413@gmail.com>
2016-08-31 10:52:54 -0500

    Added simpleSimon and weatheMap videos, added carasouel functionality as well as page indicators TODO: link pageIndicators to "pages"

--- public/interactiveResume/js/interactiveResume.js
+++ public/interactiveResume/js/interactiveResume.js
@@ -3,82 +3,149 @@
 
 //==============================================================={Curve Text}======================================================================\
 
-$('#firstHeading').arctext({radius: 300})
-$('#secondHeading').arctext({radius: 300})
-$('#thirdHeading').arctext({radius: 300, dir: -1})
-$('#fourthHeading').arctext({radius: 300})
+	$('#firstHeading').arctext({radius: 300})
+	$('#secondHeading').arctext({radius: 300})
+	$('#thirdHeading').arctext({radius: 300, dir: -1})
+	$('#fourthHeading').arctext({radius: 300})
 
-//==================================================================================================================================================\
+//========================================================={Hide Content On Page Load}==============================================================\
 
-//==============================================================={Click Functions}==================================================================\
-$('#myProjectText').hide();
-$('#careerStoryText').hide();
-$('#contactMeText').hide();
-$('#rightMenuArrow').hide();
-$('#leftMenuArrow').hide();
-$('#colorClockVideo').hide();
-
-
-$('#welcomeClick').click(function(){
-	$('#myProjectText').fadeOut();
-	$('#careerStoryText').fadeOut();
-	$('#contactMeText').fadeOut();
+	$('#myProjectText').hide();
+	$('#careerStoryText').hide();
+	$('#contactMeText').hide();
 	$('#rightMenuArrow').hide();
 	$('#leftMenuArrow').hide();
-	setTimeout(function(){
-		$('#welcomeText').fadeIn();
-	},500);
-})
-$('#myProjectsClick').click(function(){
-	$('#welcomeText').fadeOut();
-	$('#careerStoryText').fadeOut();
-	$('#contactMeText').fadeOut();
-	setTimeout(function(){
-	$('#myProjectText').fadeIn();
-	$('#rightMenuArrow').fadeIn();
-	$('#leftMenuArrow').fadeIn();
-	},500);
-	$('#calcVideo').get(0).load();
-	$('#calcVideo').get(0).play();
-})
-$('#careerStoryClick').click(function(){
-	$('#welcomeText').fadeOut();
-	$('#myProjectText').fadeOut();
-	$('#contactMeText').fadeOut();
-	$('#rightMenuArrow').hide();
-	$('#leftMenuArrow').hide();	
-	setTimeout(function(){
-	$('#careerStoryText').fadeIn();
-	},500);
-})
-$('#contactMeClick').click(function(){
-	$('#welcomeText').fadeOut();
-	$('#myProjectText').fadeOut();
-	$('#careerStoryText').fadeOut();
-	$('#rightMenuArrow').hide();
-	$('#leftMenuArrow').hide();
-	setTimeout(function(){
-	$('#contactMeText').fadeIn();	
-	},500);
-})
+	$('#colorClockVideo').hide();
+	$('#simpleSimonVideo').hide();
+	$('#weatherMapVideo').hide();
+	$('#pageIndicator1').hide();
+	$('#pageIndicator2').hide();
+	$('#pageIndicator3').hide();
+	$('#pageIndicator4').hide();
+
+//=========================================================={Functions For Page Indicators}=========================================================\
+
+	function hidePageIndicators() {
+		$('#pageIndicator1').hide();
+		$('#pageIndicator2').hide();
+		$('#pageIndicator3').hide();
+		$('#pageIndicator4').hide();
+	}
+
+	function showPageIndicators() {
+		$('#pageIndicator1').fadeIn();
+		$('#pageIndicator2').fadeIn();
+		$('#pageIndicator3').fadeIn();
+		$('#pageIndicator4').fadeIn();
+	}
+
+//=========================================================={Functions For Menu Arrows}==============================================================\
+
+	function hideMenuArrows() {
+		$('#rightMenuArrow').hide();
+		$('#leftMenuArrow').hide();
+	}
+
+	function showMenuArrows() {
+		$('#rightMenuArrow').fadeIn();
+		$('#leftMenuArrow').fadeIn();
+	}
+	var index = [
+		'#calcVideo',
+		'#colorClockVideo',
+		'#simpleSimonVideo',
+		'#weatherMapVideo'
+	]
+
+	var pageIndicators = [
+		'#pageIndicator1',
+		'#pageIndicator2',
+		'#pageIndicator3',
+		'#pageIndicator4'
+	]
+
+	var i = 0;
+
+	$('#rightMenuArrow').click(function(){
+		$(index[i]).fadeOut();
+		$(pageIndicators[i]).animate({backgroundColor: '#566246'}, '450');
+		i += 1;
+		if (i >= 4) {
+			i = 0;
+		}
+		setTimeout(function(){
+			$(index[i]).fadeIn();
+			$(pageIndicators[i]).animate({backgroundColor: '#F4D49C'}, '500');
+		},500);
+		$(index[i]).get(0).load();
+		$(index[i]).get(0).play();
+	})
+
+	$('#leftMenuArrow').click(function(){
+		$(index[i]).fadeOut();
+		$(pageIndicators[i]).animate({backgroundColor: '#566246'}, '450');
+		i -= 1;
+		if (i <= -1) {
+			i = 3;
+		}
+		setTimeout(function(){
+			$(index[i]).fadeIn();
+			$(pageIndicators[i]).animate({backgroundColor: '#F4D49C'}, '500');
+		},500);
+		$(index[i]).get(0).load();
+		$(index[i]).get(0).play();
+	})
 
 
-$('#rightMenuArrow').click(function(){
-	$('#calcVideo').fadeOut();
-	setTimeout(function(){
-	$('#colorClockVideo').fadeIn();
-	},500);
-	$('#colorClockVideo').get(0).load();
-	$('#colorClockVideo').get(0).play();
-})
-$('#leftMenuArrow').click(function(){
-	$('#colorClockVideo').fadeOut();
-	setTimeout(function(){
-	$('#calcVideo').fadeIn();
-	},500);
-	$('#calcVideo').get(0).load();
-	$('#calcVideo').get(0).play();
-})
+
+//==========================================================={Function For Page Indicators}==========================================================\
+
+//============================================================={Click Functions For Content}=========================================================\
+	$('#welcomeClick').click(function(){
+		$('#myProjectText').fadeOut();
+		$('#careerStoryText').fadeOut();
+		$('#contactMeText').fadeOut();
+		hideMenuArrows();
+		hidePageIndicators();
+		setTimeout(function(){
+			$('#welcomeText').fadeIn();
+		},500);
+	})
+	$('#myProjectsClick').click(function(){
+		$('#welcomeText').fadeOut();
+		$('#careerStoryText').fadeOut();
+		$('#contactMeText').fadeOut();
+		setTimeout(function(){
+		$('#myProjectText').fadeIn();
+		showMenuArrows();
+		showPageIndicators();
+		$(pageIndicators[i]).css('background', '#F4D49C');
+		},500);
+		$('#calcVideo').get(0).load();
+		$('#calcVideo').get(0).play();
+	})
+	$('#careerStoryClick').click(function(){
+		$('#welcomeText').fadeOut();
+		$('#myProjectText').fadeOut();
+		$('#contactMeText').fadeOut();
+		hideMenuArrows();
+		hidePageIndicators();
+		setTimeout(function(){
+		$('#careerStoryText').fadeIn();
+		},500);
+	})
+	$('#contactMeClick').click(function(){
+		$('#welcomeText').fadeOut();
+		$('#myProjectText').fadeOut();
+		$('#careerStoryText').fadeOut();
+		hideMenuArrows();
+		hidePageIndicators();
+		setTimeout(function(){
+		$('#contactMeText').fadeIn();	
+		},500);
+	})
+
+
 
 
 
