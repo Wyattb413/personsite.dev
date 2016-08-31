30fb50eb0d8eaa70ee185b8dad7b5be9a4b12adf
Wyatt Barnes <wyattb413@gmail.com>
2016-08-31 14:45:52 -0500

    Added dynamic video overlays, dynamic page indicators TODO: turn page overlays into modals

--- public/interactiveResume/js/interactiveResume.js
+++ public/interactiveResume/js/interactiveResume.js
@@ -22,6 +22,10 @@
 	$('#pageIndicator2').hide();
 	$('#pageIndicator3').hide();
 	$('#pageIndicator4').hide();
+	$('#videoOverlayCalc').hide();
+	$('#videoOverlayColorClock').hide();
+	$('#videoOverlaySimpleSimon').hide();
+	$('#videoOverlayWeatherMap').hide();
 
 //=========================================================={Functions For Page Indicators}=========================================================\
 
@@ -50,24 +54,33 @@
 		$('#rightMenuArrow').fadeIn();
 		$('#leftMenuArrow').fadeIn();
 	}
+
 	var index = [
 		'#calcVideo',
 		'#colorClockVideo',
 		'#simpleSimonVideo',
 		'#weatherMapVideo'
-	]
+	];
 
 	var pageIndicators = [
 		'#pageIndicator1',
 		'#pageIndicator2',
 		'#pageIndicator3',
 		'#pageIndicator4'
+	];
+
+	var videoOverlays = [
+		'#videoOverlayCalc',
+		'#videoOverlayColorClock',
+		'#videoOverlaySimpleSimon',
+		'#videoOverlayWeatherMap'
 	]
 
 	var i = 0;
 
 	$('#rightMenuArrow').click(function(){
 		$(index[i]).fadeOut();
+		$(videoOverlays[i]).fadeOut();
 		$(pageIndicators[i]).animate({backgroundColor: '#566246'}, '450');
 		i += 1;
 		if (i >= 4) {
@@ -75,14 +88,16 @@
 		}
 		setTimeout(function(){
 			$(index[i]).fadeIn();
+			$(videoOverlays[i]).fadeIn();
 			$(pageIndicators[i]).animate({backgroundColor: '#F4D49C'}, '500');
 		},500);
 		$(index[i]).get(0).load();
 		$(index[i]).get(0).play();
-	})
+	});
 
 	$('#leftMenuArrow').click(function(){
 		$(index[i]).fadeOut();
+		$(videoOverlays[i]).fadeOut();
 		$(pageIndicators[i]).animate({backgroundColor: '#566246'}, '450');
 		i -= 1;
 		if (i <= -1) {
@@ -90,13 +105,76 @@
 		}
 		setTimeout(function(){
 			$(index[i]).fadeIn();
+			$(videoOverlays[i]).fadeIn();
 			$(pageIndicators[i]).animate({backgroundColor: '#F4D49C'}, '500');
 		},500);
 		$(index[i]).get(0).load();
 		$(index[i]).get(0).play();
-	})
+	});
 
+	$('#pageIndicator1').click(function(){
+		$(index[i]).fadeOut();
+		$(videoOverlays[i]).fadeOut();
+		$(pageIndicators[i]).animate({backgroundColor: '#566246'}, '450');
+		i =0
+		setTimeout(function(){
+			$(index[i]).fadeIn();
+			$(videoOverlays[i]).fadeIn();
+			$(pageIndicators[i]).animate({backgroundColor: '#F4D49C'}, '500');
+		},500);
+		$(index[i]).get(0).load();
+		$(index[i]).get(0).play();
+	});
+	$('#pageIndicator2').click(function(){
+		$(index[i]).fadeOut();
+		$(videoOverlays[i]).fadeOut();
+		$(pageIndicators[i]).animate({backgroundColor: '#566246'}, '450');
+		i =1
+		setTimeout(function(){
+			$(index[i]).fadeIn();
+			$(videoOverlays[i]).fadeIn();
+			$(pageIndicators[i]).animate({backgroundColor: '#F4D49C'}, '500');
+		},500);
+		$(index[i]).get(0).load();
+		$(index[i]).get(0).play();
+	});
+	$('#pageIndicator3').click(function(){
+		$(index[i]).fadeOut();
+		$(videoOverlays[i]).fadeOut();
+		$(pageIndicators[i]).animate({backgroundColor: '#566246'}, '450');
+		i =2
+		setTimeout(function(){
+			$(index[i]).fadeIn();
+			$(videoOverlays[i]).fadeIn();
+			$(pageIndicators[i]).animate({backgroundColor: '#F4D49C'}, '500');
+		},500);
+		$(index[i]).get(0).load();
+		$(index[i]).get(0).play();
+	});
+	$('#pageIndicator4').click(function(){
+		$(index[i]).fadeOut();
+		$(videoOverlays[i]).fadeOut();
+		$(pageIndicators[i]).animate({backgroundColor: '#566246'}, '450');
+		i =3
+		setTimeout(function(){
+			$(index[i]).fadeIn();
+			$(videoOverlays[i]).fadeIn();
+			$(pageIndicators[i]).animate({backgroundColor: '#F4D49C'}, '500');
+		},500);
+		$(index[i]).get(0).load();
+		$(index[i]).get(0).play();
+	});
 
+	function hideVideoOverlays() {
+		$('#videoOverlayCalc').hide();
+		$('#videoOverlayColorClock').hide();
+		$('#videoOverlaySimpleSimon').hide();
+		$('#videoOverlayWeatherMap').hide();
+	}
+
+	function showVideoOverlay() {
+		$(videoOverlays[i]).fadeIn();
+	}
 
 //==========================================================={Function For Page Indicators}==========================================================\
 
@@ -105,54 +183,68 @@
 		$('#myProjectText').fadeOut();
 		$('#careerStoryText').fadeOut();
 		$('#contactMeText').fadeOut();
+		hideVideoOverlays();
 		hideMenuArrows();
 		hidePageIndicators();
 		setTimeout(function(){
 			$('#welcomeText').fadeIn();
 		},500);
-	})
+	});
 	$('#myProjectsClick').click(function(){
 		$('#welcomeText').fadeOut();
 		$('#careerStoryText').fadeOut();
 		$('#contactMeText').fadeOut();
 		setTimeout(function(){
 		$('#myProjectText').fadeIn();
+		$('#videoOverlayCalc').fadeIn();
+		showVideoOverlay();
 		showMenuArrows();
 		showPageIndicators();
 		$(pageIndicators[i]).css('background', '#F4D49C');
 		},500);
 		$('#calcVideo').get(0).load();
 		$('#calcVideo').get(0).play();
-	})
+	});
 	$('#careerStoryClick').click(function(){
 		$('#welcomeText').fadeOut();
 		$('#myProjectText').fadeOut();
 		$('#contactMeText').fadeOut();
+		hideVideoOverlays();
 		hideMenuArrows();
 		hidePageIndicators();
 		setTimeout(function(){
 		$('#careerStoryText').fadeIn();
 		},500);
-	})
+	});
 	$('#contactMeClick').click(function(){
 		$('#welcomeText').fadeOut();
 		$('#myProjectText').fadeOut();
 		$('#careerStoryText').fadeOut();
+		hideVideoOverlays();
 		hideMenuArrows();
 		hidePageIndicators();
 		setTimeout(function(){
 		$('#contactMeText').fadeIn();	
 		},500);
-	})
+	});
+
+
+$('#videoOverlayCalc').click(function(){
+	alert('Calc');
+});
+$('#videoOverlayColorClock').click(function(){
+	alert('ColorClock');
+});
+$('#videoOverlaySimpleSimon').click(function(){
+	alert('SimpleSimon');
+});
+$('#videoOverlayWeatherMap').click(function(){
+	alert('WeatherMap');
+});
 
 
 
-
-
-
-
-
-
+//current varible that changes every "page" load
 
 
 
