3d664327e4b02b87ad5133e4db5425eaef19b909
Wyatt Barnes <wyattb413@gmail.com>
2016-09-13 16:46:21 -0500

    Basic html5 structure (ignore commented out code...doing this for git tracking purposes)

---
+++ public/xyz.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Welcome!</title>
+	<!==============================================================={BootStrap V3.3.7}=============================================================\>
+		<!-- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" 
+		integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
+	<!==============================================================={Custom CSS}===================================================================\>
+		<link rel="stylesheet" type="text/css" href="/xyz/css/xyz.css">
+
+		<!-- <link href="/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+	    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+	    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+	    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'> -->
+
+
+	</head>
+	<body>
+		<!-- <div class="header" id="page-top">
+			<div class="navBar">
+				<a href="#"><div class="" id="nameText">Wyatt Barnes</div></a>
+				<a href="#"><div class="navBarText" id="contactMeText">• Contact Me •</div></a>
+				<a href="#"><div class="navBarText" id="careerStoryText"> • My Resume • </div></a>
+				<a href="#"><div class="navBarText" id="myProjectsText">• My Portfolio •</div></a>
+			</div> -->
+			<!-- <div class="logoContainer"> -->
+				<!-- <img src="/xyz/img/logoComplete2.png" id="imageLogo"> -->
+			<!-- </div> -->
+		<!-- </div> -->
+
+		<!-- <section class="parallax">
+
+			<div class="parallax-inner">
+			
+				<h2>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aspernatur magnam, tenetur eaque, corporis iste voluptate, qui dolores, omnis pariatur cupiditate voluptas vero ipsam illo. Similique quis quam, laboriosam nulla.
+				</h2>
+
+			</div>	
+
+		</section> -->
+
+		<!-- <h1>Random 3</h1> -->
+
+
+
+
+
+
+
+	<!/=============================================================={Jquery V2.1.3}================================================================\>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<!//==============================================================={Jquery Ui V1.12}============================================================\>
+		<script   src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"   integrity="sha256-0YPKAwZP7Mp3ALMRVB2i8GXeEndvCq3eSl/WsAl1Ryk="   crossorigin="anonymous"></script>
+	<!//==================================================={BootStrap JavaScript V3.3.7}============================================================\>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<!//==============================================================={Animated Modal}=============================================================\>
+		<script src="/interactiveResume/js/animatedModal.min.js"></script>
+	<!//============================================================={Custom JavaScript}============================================================\>
+		<script src="/xyz/js/xyz.js"></script>
+	</body>
+</html>
