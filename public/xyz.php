ecdbe780443e233d97d7288b37d443c6257d3bb8
Wyatt Barnes <wyattb413@gmail.com>
2016-09-16 08:39:03 -0500

    Recreated parallax effect using Stellar library KNOWN ISSUES: Safari hates parallax POSSIBLE FIXES: Disable parallax on safari :(

--- public/xyz.php
+++ public/xyz.php
@@ -1,63 +1,65 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Welcome!</title>
-	<!==============================================================={BootStrap V3.3.7}=============================================================\>
-		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" 
-		integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<!==============================================================={Custom CSS}===================================================================\>
-		<link rel="stylesheet" type="text/css" href="/xyz/css/xyz.css">
-
-		<link href="/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-	    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-	    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
-	    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-	    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
-
-
-	</head>
-	<body>
-		<div class="header" id="page-top">
-			<div class="navBar">
-				<a href="#"><div class="" id="nameText">Wyatt Barnes</div></a>
-				<a href="#"><div class="navBarText" id="contactMeText">• Contact Me •</div></a>
-				<a href="#"><div class="navBarText" id="careerStoryText"> • My Resume • </div></a>
-				<a href="#"><div class="navBarText" id="myProjectsText">• My Portfolio •</div></a>
-			</div>
-			<div class="logoContainer">
-				<img src="/xyz/img/logoComplete2.png" id="imageLogo">
-			</div>
-		</div>
-
-		<section class="parallax">
-
-			<div class="parallax-inner">
-			
-				<h2>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aspernatur magnam, tenetur eaque, corporis iste voluptate, qui dolores, omnis pariatur cupiditate voluptas vero ipsam illo. Similique quis quam, laboriosam nulla.
-				</h2>
-
-			</div>	
-
-		</section>
-
-		<!-- <h1>Random 3</h1> -->
+  <head>
+    <title>Welcome!</title>
+  <!==============================================================={BootStrap V3.3.7}=============================================================\>
+      <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" 
+      integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <!==============================================================={Custom CSS}===================================================================\>
+      <link rel="stylesheet" type="text/css" href="/xyz/css/xyz.css">
+  <!==============================================================={Custom Fonts}=================================================================\>
+      <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+      <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+      <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+      <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+    <!/=============================================================={Jquery V2.1.3}==============================================================\>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <!/=============================================================={Stellar Js}=================================================================\>
+      <script src="/xyz/js/jquery.stellar.min.js"></script>
+      <script>
+        $(document).ready(function(){
+          $(window).stellar();
+        });
+      </script>
 
 
+  </head>
+    <body>
+      <!-- <div id="customNav">
+        <div id="customlogo"><img src="/xyz/img/logoComplete2.png"></div>
+      </div> -->
+
+      <div id="wall_1" class="image" data-stellar-background-ratio=".3"></div>
+      <div id="content_1" class="content">
+        <h1>Heading</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil consequuntur facere minima nostrum officiis atque deserunt eius esse veniam nobis maxime repudiandae voluptas, aperiam corporis dolorem maiores corrupti pariatur necessitatibus!</p>
+      </div>
+
+      <div id="wall_2" class="image" data-stellar-background-ratio=".3"></div>
+      <div id="content_2" class="content">
+        <h1>Heading</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil consequuntur facere minima nostrum officiis atque deserunt eius esse veniam nobis maxime repudiandae voluptas, aperiam corporis dolorem maiores corrupti pariatur necessitatibus!</p>
+      </div>
+
+      <div id="wall_3" class="image" data-stellar-background-ratio=".3"></div>
+      <div id="content_3" class="content">
+        <h1>Heading</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil consequuntur facere minima nostrum officiis atque deserunt eius esse veniam nobis maxime repudiandae voluptas, aperiam corporis dolorem maiores corrupti pariatur necessitatibus!</p>
+      </div>
 
 
 
 
 
-	<!/=============================================================={Jquery V2.1.3}================================================================\>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<!//==============================================================={Jquery Ui V1.12}============================================================\>
-		<script   src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"   integrity="sha256-0YPKAwZP7Mp3ALMRVB2i8GXeEndvCq3eSl/WsAl1Ryk="   crossorigin="anonymous"></script>
-	<!//==================================================={BootStrap JavaScript V3.3.7}============================================================\>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<!//==============================================================={Animated Modal}=============================================================\>
-		<script src="/interactiveResume/js/animatedModal.min.js"></script>
-	<!//============================================================={Custom JavaScript}============================================================\>
-		<script src="/xyz/js/xyz.js"></script>
-	</body>
+
+
+    <!//==============================================================={Jquery Ui V1.12}============================================================\>
+      <script   src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"   integrity="sha256-0YPKAwZP7Mp3ALMRVB2i8GXeEndvCq3eSl/WsAl1Ryk="   crossorigin="anonymous"></script>
+    <!//==================================================={BootStrap JavaScript V3.3.7}============================================================\>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!//==============================================================={Animated Modal}=============================================================\>
+      <script src="/interactiveResume/js/animatedModal.min.js"></script>
+    <!//============================================================={Custom JavaScript}============================================================\>
+      <script src="/xyz/js/xyz.js"></script>
+    </body>
 </html>
