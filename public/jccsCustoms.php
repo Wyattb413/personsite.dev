42fae9813f159239ae27badfd0b35648210ca181
Wyatt Barnes <wyattb413@gmail.com>
2016-09-18 17:56:20 -0500

    Starting Website Design For Family Friend To Show Off Custom Paint Work

---
+++ public/jccsCustoms.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Building One Bike At A Time</title>
+		<link rel="stylesheet" type="text/css" href="/jccsCustoms/jccsCustoms.css">
+		<link href="https://fonts.googleapis.com/css?family=Cormorant+SC" rel="stylesheet">
+	</head>
+	<body>
+	<!-- <div class="welcomeTextDiv"> -->
+		<span class="welcomeText">JCCS Customs</span>
+		<span class="phoneNumber">(210)-289-7028/2498</span>
+	<!-- </div> -->
+	<div class="fancyBreak"></div>
+		<div class="overlay">
+			
+		</div>
+
+
+
+
+
+
+
+	</body>
+</html>
