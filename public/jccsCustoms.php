8abcfc52c01fca8ba6038bac4edf9606a92aaa6c
Wyatt Barnes <wyattb413@gmail.com>
2016-09-20 15:51:05 -0500

    Jccs Customs Website (Should really commit more often)

--- public/jccsCustoms.php
+++ public/jccsCustoms.php
@@ -3,23 +3,51 @@
 	<head>
 		<title>Building One Bike At A Time</title>
 		<link rel="stylesheet" type="text/css" href="/jccsCustoms/jccsCustoms.css">
-		<link href="https://fonts.googleapis.com/css?family=Cormorant+SC" rel="stylesheet">
+		<!-- <link href="https://fonts.googleapis.com/css?family=Cormorant+SC" rel="stylesheet"> -->
 	</head>
 	<body>
-	<!-- <div class="welcomeTextDiv"> -->
-		<span class="welcomeText">JCCS Customs</span>
-		<span class="phoneNumber">(210)-289-7028/2498</span>
-	<!-- </div> -->
-	<div class="fancyBreak"></div>
-		<div class="overlay">
-			
+		<div id="fb-root"></div>
+		<script>(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7";
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
+
+		<div class="overlay"></div>
+		<header>
+			<div class=""><img src="/jccsCustoms/img/jccsCustomsLogo.png" class="logo header-left"></div>
+			<div class="header-center">Building One Bike At A Time</div>
+			<div class="header-right">(210)-289-7028/2498</div>
+		</header>
+		<div class="mainImgContainer">
+			<div class="mainImgDiv">
+				<div class="mainImg">
+					<img src="/jccsCustoms/img/greyBike.jpeg" class="greyBike">
+				</div>
+			</div>
+		</div>
+		<div class="menuBarDiv">
+			<div class="menuBar">
+				<span class="completeBuildsLink"><a href="#" class="links">Complete Builds</a></span>
+				<span class="customPaintJobsLink"><a href="#" class="links">Custom Paint Jobs</a></span>
+				<span class="customDesignsLink"><a href="#" class="links">Custom Designs Link</a></span>
+			</div>
+		</div>
+		<div class="completeBuilds">
+			<img src="/jccsCustoms/img/completeBuild1.jpeg" class="completeBuild1 completeBuildImg">
+			<img src="/jccsCustoms/img/completeBuild2.jpeg" class="completeBuild2 completeBuildImg">
+			<img src="/jccsCustoms/img/completeBuild3.jpeg" class="completeBuild3 completeBuildImg">
+			<img src="/jccsCustoms/img/completeBuild4.jpeg" class="completeBuild4 completeBuildImg">
+		</div>
+		<div class="facebookFeed">
+			<div class="fb-page" data-href="https://www.facebook.com/JccsCustoms/" data-tabs="timeline" data-width="300" data-height="1000" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/JccsCustoms/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/JccsCustoms/"></a></blockquote></div>
 		</div>
 
 
 
-
-
-
-
+		<script src="https://unpkg.com/masonry-layout@4.1/dist/masonry.pkgd.min.js"></script>
+		<script src="/jccsCustoms/jccsCustoms.js"></script>
 	</body>
 </html>
